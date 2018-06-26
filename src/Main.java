5942cf108375fb39eb4a66cb04df9bc67ce131f1
panda503 <joshuabsa503@gmail.com>
2018-06-26 15:17:25 -0400

    string

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello world");
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter a word to see if it is a palindrome");
+        String Palindrome = scanner.nextLine();
+        if (isPalindrome(Palindrome)){
+            System.out.println("that is a palindrome");
+        }
+        else{
+            System.out.println("that is not a palindrome");
+        }
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String input){
+        int half = (int)Math.floor(input.length() / 2D);
+        input = input.toUpperCase();
+        String[] a = input.split(regex:"")
+        for(int i = 0; i < half ; ++i){
+            if(input.charAt(i) != input.charAt((input.length()-1)-i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
