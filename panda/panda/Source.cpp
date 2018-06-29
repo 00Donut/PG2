ee0425c77b70d66684c671bee778e7bf35a778bf
panda503 <joshuabsa503@gmail.com>
2018-06-29 14:02:43 -0400

    c#

--- panda/panda/Source.cpp
+++ panda/panda/Source.cpp
@@ -2,27 +2,47 @@
 #include <string>
 #include <random>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
 void selection_sort(vector<int>&);
 
 void swap(int&, int&);
-
-int main(int argc, char** argv) {
+/*int main(int argc, char** argv) {
 	random_device generator;
 	uniform_int_distribution<int> dist(0, 1000);
 	vector <int> list;
-	for (int i = 0; i < 10; ++i) {
+	for (size_t i = 0; i < 10; ++i) {
 		list.push_back(dist(generator));
 		cout << list[i] << ", ";
 	}
 	cout << endl;
 	selection_sort(list);
-	for (int i = 0; i < 10; ++i) {
+	for (size_t i = 0; i < 10; ++i) {
 		cout << list[i] << ", ";
 	}
 	selection_sort(list);
+	system("pause");
+	return 0;
+}
+*/
+int main(int argc, char** argv) {
+	ofstream fout;
+	fout.open("output.text");
+	if (fout.fail()) {
+		return 1;
+	}
+	fout << "Hello!" << endl;
+	fout.close();
+	ifstream fin;
+	fin.open("input.txt", ios::app);
+	if (fin.fail()) {
+		return 1;
+	}
+	string a;
+	fin >> a;
+	cout << a << endl;
 	system("pause");
 	return 0;
 }
